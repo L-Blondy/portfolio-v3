2ddc1ca479dba4eb24f092022dbd6a6981588155
laurent blondy <blondy.laurent@cenozai.local>
2021-09-06 17:18:51 +0200

    update resume link

--- src/components/ResumeLink.tsx
+++ src/components/ResumeLink.tsx
@@ -9,5 +9,5 @@ export const ResumeLink: React.FC<Props> = ({
 	children = null,
 	className = '',
 }) => (
-	<Link href='/curriculum_laurent-blondy_ENG.pdf'><a className={className} hrefLang='en' rel='noopener noreferrer' target='_blank'>{children}</a></Link>
+	<Link href='/curriculum-laurent_blondy-ENG.pdf'><a className={className} hrefLang='en' rel='noopener noreferrer' target='_blank'>{children}</a></Link>
 )
